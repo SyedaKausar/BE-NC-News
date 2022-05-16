7700951519724e146bf4a8281cbcf6da02166fd9
SyedaKausar <sfkausar@gmail.com>
2022-05-16 16:11:36 +0100

    getArticle

--- controllers/topics.controllers.js
+++ controllers/topics.controllers.js
@@ -1,8 +1,14 @@
-const { fetchTopics } = require("../models/topics.models");
+const { fetchTopics, fetchArticle } = require("../models/topics.models");
 
 exports.getTopics = (req, res) => {
   fetchTopics().then((topics) => {
-      console.log(topics)
     res.status(200).send({ topics });
+  });
+};
+exports.getArticle = (req, res) => {
+  const id = parseInt(req.params.article_id);
+ // console.log(id, typeof id);
+  fetchArticle(id).then((article) => {
+    res.status(200).send({ article });
   });
 };
--- models/topics.models.js
+++ models/topics.models.js
@@ -4,3 +4,11 @@ exports.fetchTopics = () => {
     return result.rows;
   });
 };
+exports.fetchArticle = (id) => {
+  return db
+    .query("SELECT * FROM articles WHERE article_id = $1", [id])
+    .then((result) => {
+      //console.log(result.rows);
+      return result.rows[0];
+    });
+};
